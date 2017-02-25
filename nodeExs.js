6106ba70c561f788a673cc7d3045b9a33cfcfbcc
madeleinel <madeleinelinder@hotmail.com>
2017-02-25 11:44:45 +0000

    ex1 and ex2

---
+++ nodeExs.js
@@ -0,0 +1,22 @@
+// Exercise 1
+// console.log("HELLO WORLD");
+
+// // run by 'node nodeExs'
+// // verify exercise by 'learnyounode verify nodeExs'
+// // to run in test environment: 'learnyounode run'
+// // 'learnyounode print' or 'learnyounode help'
+
+//Exercise 2
+// var data = process.argv;
+// var dataLength = data.length;
+// var tot = 0;
+//
+// for (i = 2; i < dataLength; i++) {
+//   // var num = Number(data[i]);
+//   // tot = tot + num;
+//   tot += Number(data[i]);
+// }
+//
+// console.log(tot);
+
+// Exercise 3
